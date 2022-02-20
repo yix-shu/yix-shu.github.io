366ba61dde1b865e4f8cc7a7099f61b4a1e7f3c8
yix-shu <yix.shu723@gmail.com>
2022-02-19 19:11:04 -0800

    Debugging php file

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
+    extract($_REQUEST);
+    $file = fopen("messages.txt", "a");
 
-$file = fopen("messages.txt", "a");
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
-
-fwrite($file, "name: ");
-fwrite($file, $name)
-fwrite($$file, "email: ");
-fwrite($file, $email)
-fwrite($file, "message: ");
-fwrite($file, $message);
-fwrite($file, "\n");
-fclose($file);
+    fwrite($file, "name: ");
+    fwrite($file, $name)
+    fwrite($$file, "email: ");
+    fwrite($file, $email)
+    fwrite($file, "message: ");
+    fwrite($file, $message);
+    fwrite($file, "\n");
+    fclose($file);
 ?>
